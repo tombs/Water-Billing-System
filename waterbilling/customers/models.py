6788a3d596bcc52ca7427ab706424ef46b070847
Edwin Tumbaga <edwin.tumbaga@gmail.com>
2016-07-21 12:43:08 +0800

    - added correct model components

--- waterbilling/customers/models.py
+++ waterbilling/customers/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 # Create your models here.
-from core.models import Customers
-from core.models import Addresses
+from core.models import Customer, Address, Account, Payment, Adjustment, PostedPayment, FinancialTransaction
+
